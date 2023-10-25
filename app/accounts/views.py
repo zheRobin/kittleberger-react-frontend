1446d006768e16bebb889baee4daaf5e08a8f7e0
kostiantynzvir <zvirkost@outlook.com>
2023-10-25 18:56:57 -0400

    xml-parsing: get json data manually

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
-from app.util import *
+from master.util import *
 from accounts.serializers import *
 import re
 import jwt
--- app/master/views.py
+++ app/master/views.py
@@ -3,59 +3,85 @@ from django.core.files.base import ContentFile
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from pymongo import MongoClient
-from gridfs import GridFSBucket
 import zipfile
-import json, xmltodict
-from bson import ObjectId
+import json
+import os
+import pymongo.errors
+import time
+from master.models import *
 from lxml import etree
 from lxml.etree import XMLSyntaxError
-from app.util import *
-from master.models import *
 import environ
-import pymongo.errors
-import os
+from master.util import *
 env = environ.Env()
 environ.Env.read_env()
-# Create your views here.
+
 class parseAPIView(APIView):
     def post(self, request):
         file = request.FILES['file']
-        filepath = default_storage.save(os.path.join('temp', file.name) , ContentFile(file.read()))
+        filepath = default_storage.save(os.path.join('temp', file.name), ContentFile(file.read()))
+
         try:
             client = MongoClient(host=env('MONGO_DB_HOST'))
             db = client[env('MONGO_DB_NAME')]
-            bucket = GridFSBucket(db) 
+
+            collection_name = str(int(time.time()))
+            collection = db[collection_name]
+            def process(context):
+                for event, elem in context:
+                    try:
+                        data = convert(elem)
+                        json_data = json.dumps(data)
+                        collection.insert_one(json.loads(json_data))
+                    except json.JSONDecodeError:
+                        print("Failed to decode JSON")
+                    except Exception as e:
+                        print(f"Unexpected error: {str(e)}")
+
+                    finally:
+                        elem.clear()
+                        while elem.getprevious() is not None:
+                            del elem.getparent()[0]
+                        Document.objects.create(file_id=collection_name)
+
         except pymongo.errors.ServerSelectionTimeoutError as err:
-            return Response(error(self, "Unable to connect to MongoDB"))        
+            return Response(error(self, "Unable to connect to MongoDB"))
 
         if not zipfile.is_zipfile(filepath):
             return Response(error(self, "Not a valid zip file"))
 
-        # Unzip the file
         with zipfile.ZipFile(filepath, 'r') as zip_ref:
             errors = []
             for f in zip_ref.namelist():
-                if f.endswith('.xml'):  # Check if it's an XML
+                if f.endswith('.xml'):
                     try:
-                        xml_doc = etree.parse(zip_ref.open(f))
-                        data_dict = xmltodict.parse(etree.tostring(xml_doc))
-                        json_data_bytes = json.dumps(data_dict).encode('utf-8')
-                        file_id = bucket.upload_from_stream(f,json_data_bytes)
-                        Document.objects.create(file_id=file_id)
+                        start_time = time.time()
+                        context = etree.iterparse(zip_ref.open(f), events=('end',), tag='mediaobject')
+                        process(context)
+                        end_time = time.time()
+                        duration = end_time - start_time
+                        print(f'Time taken: {duration} seconds')
                     except XMLSyntaxError:
                         errors.append(f"File {f} is not a well-formed XML document.")
                     except Exception as e:
-                        return Response(error(self, "Unable to insert data into MongoDB"))
-            print (errors)
-        zip_ref.close() 
+                        return Response(error(self, str(e)))
+
+        zip_ref.close()
         os.remove(filepath)
+
+        if errors.__len__() > 0:
+            return Response(error(self, errors))
         return Response(success(self, "Data inserted successfully"))
-    
+
     def get(self, request):
-        file_id = request.GET['file_id']
-        client = MongoClient(host=env('MONGO_DB_HOST'))
-        db = client[env('MONGO_DB_NAME')]
-        bucket = GridFSBucket(db)
-        grid_in = bucket.open_download_stream(ObjectId(file_id)).read()
-        return Response(success(self, json.loads(grid_in)))
-        
+        try:
+            file_id = request.GET['file_id']
+            client = MongoClient(host=env('MONGO_DB_HOST'))
+            db = client[env('MONGO_DB_NAME')]
+        except pymongo.errors.ServerSelectionTimeoutError as err:
+            return Response(error(self, "Unable to connect to MongoDB"))
+
+        # Retrieve the latest_collection from PostgreSQL
+        document = Document.objects.latest('id')
+        data = db[document.latest_collection].find_one({"_id": file_id})
+        return Response(success(self, json.loads(data))) 
