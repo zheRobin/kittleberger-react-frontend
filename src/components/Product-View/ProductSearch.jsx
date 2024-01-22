f24e5f0b9c733ff80945c6e307896962e74b55ce
Kostiantyn <kostiantynzvir@gmail.com>
2024-01-22 09:22:10 -0500

    fix: product search with init page as 1

--- src/components/Product-View/ProductSearch.jsx
+++ src/components/Product-View/ProductSearch.jsx
@@ -34,7 +34,7 @@ export default function ProductSearch() {
   const handleChange = (event, value) => {
     const selected = value.map(val => val.article_number);
     const updatedFilter = { ...filter, article_list: selected };
-    dispatch(infoActions.initProductData());
+    dispatch(infoActions.setProductPage(1));
     dispatch(infoActions.setFilterData(updatedFilter));
     setData(value)
   };
